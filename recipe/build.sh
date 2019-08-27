cd618ed667e11d709b790d4711b1f8c4926d3112
Julien Schueller <schueller@phimeca.com>
2019-08-27 09:34:03 +0200

    Simplify build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,8 @@ if [[ `uname -s` == 'Darwin' ]]; then
 fi
 
 mkdir _build && cd _build
-cmake .. \
+cmake \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_BUILD_TYPE=Release \
     -DCGNS_BUILD_SHARED=$_build_shared \
@@ -17,16 +18,12 @@ cmake .. \
     -DCGNS_ENABLE_LFS=ON \
     -DCMAKE_C_FLAGS:STRING=-D_LARGEFILE64_SOURCE \
     -DCGNS_ENABLE_HDF5=ON \
-    -DHDF5_LIBRARY=$PREFIX/lib/libhdf5$SHLIB_EXT \
-    -DHDF5_INCLUDE_PATH=$PREFIX/include \
     -DHDF5_NEED_SZIP=OFF \
     -DHDF5_NEED_ZLIB=ON \
-    -DZLIB_LIBRARY=$PREFIX/lib/libz$SHLIB_EXT
-
+    ..
 sed -i.orig 's@^c@!c@' src/cgnslib_f.h
 
-make -j$CPU_COUNT
+make install -j$CPU_COUNT
 if [[ `uname -s` == 'Linux' ]]; then
     ctest
 fi
-make install
