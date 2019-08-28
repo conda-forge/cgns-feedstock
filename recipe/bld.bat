62a8100e1c3b4278258f994e932191ef0c4aa1e3
Julien Schueller <schueller@phimeca.com>
2019-08-28 12:17:10 +0200

    Enable windows build

---
+++ recipe/bld.bat
@@ -0,0 +1,12 @@
+
+mkdir build && cd build
+cmake -LAH -G"NMake Makefiles" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DBUILD_SHARED_LIBS=ON ^
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    ..
+if errorlevel 1 exit 1
+
+cmake --build . --target install --config Release
+if errorlevel 1 exit 1
