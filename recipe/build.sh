7c5d8bebc163f2fd64b7aa4eaa3bc0146458c4dc
Julien Schueller <schueller@phimeca.com>
2020-01-22 11:54:58 +0100

    Use shared libs on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,11 @@
 #! /usr/bin/env bash
 
-_build_shared=ON
-if [[ `uname -s` == 'Darwin' ]]; then
-    _build_shared=OFF
-fi
-
 mkdir _build && cd _build
 cmake \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCGNS_BUILD_SHARED=$_build_shared \
+    -DCGNS_BUILD_SHARED=ON \
     -DCGNS_USE_SHARED=ON \
     -DCGNS_ENABLE_FORTRAN=OFF \
     -DFORTRAN_NAMING="LOWERCASE_" \
